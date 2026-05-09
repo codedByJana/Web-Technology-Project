21b9215c42fbbcd0f55ab66666e2df1e7e50afee
codedByJana <2401929@student.eelu.edu.eg>
2026-05-10 01:37:12 +0300

    fix: workshop date label

--- script.js
+++ script.js
@@ -98,15 +98,21 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
             const dateInput = document.getElementById('workshop-date');
-            
+
             if (dateInput) {
                 if (workshopDate) {
                     let date = new Date(workshopDate);
                     if (!isNaN(date.getTime())) {
-                        dateInput.value = date.toISOString().split('T')[0];
+                        //bad practice : dateInput.value = date.toISOString().split('T')[0];
+
+                        const year = date.getFullYear();
+                        const month = String(date.getMonth() + 1).padStart(2, '0');
+                        const day = String(date.getDate()).padStart(2, '0');
+
+                        dateInput.value = `${year}-${month}-${day}`;
                     }
                 } else {
-                    dateInput.value = "";
+                    dateInput.value = "TBD";
                 }
             }
             regModal.showModal();
